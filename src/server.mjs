4346a17ac27061ac3694374f480760419087c4b1
petersem <matthew.petersem.68@gmail.com>
2026-04-19 10:31:26 +1000

    FIX: implement rate limiting with white list

--- src/server.mjs
+++ src/server.mjs
@@ -29,18 +29,25 @@ app.use(express.urlencoded({ extended: true }));  // handle querystring nested d
 app.use(express.static(path.join(import.meta.dirname, "public"))); // handle any static images, stylesheets, etc.
 
 // implement express rate limiter
-const limiter = rateLimit({
+const allowList = ['localhost', '127.0.0.1', '::1'] // whitelist for local address calls
+const limiterOptions = {
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    handler: (req, res, next, options) => { // custom message for limit exceeded
+        console.log(logWarning, `Rate limit execeeded for ${req.ip.replace('::ffff:','')}`);
+        return res.status(options.statusCode).send(`Rate limit execeeded for ${req.ip.replace('::ffff:','')}. ${options.message} (after ${options.windowMs/60/1000} minutes)`);
+    },
+    skip: (req, res) => allowList.includes(req.ip.replace('::ffff:','')) // use whitelist
 	// store: ... , // Redis, Memcached, etc. See below.
-})
+}
+const limiter = rateLimit(limiterOptions)
 
 // Apply the rate limiting middleware to all requests.
 app.use(limiter)
-console.log(logInfo, `Express-rate-limiter enabled`);
+console.log(logInfo, `Express-rate-limiter enabled. Requests: ${limiterOptions.limit} Period: ${limiterOptions.windowMs / 1000 / 60} minutes`);
 
 // add cors support
 var corsOptions = {
