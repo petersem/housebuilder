d5279e8d4b021703be5df69800e576b3f0369a1c
petersem <matthew.petersem.68@gmail.com>
2026-04-21 12:51:22 +1000

    FIX: broken datamodel

--- src/models/DataModel.mjs
+++ src/models/DataModel.mjs
@@ -1,53 +1,91 @@
-/**
- * @file CompanyController.test.mjs
- */
+export class DataModel {
+    /**
+     * In memory data store of model entries
+     */
+    static data = null;
+    
+    static setDataSource(data) {
+        this.data = data;
+    }
+    
+    /**
+     * Find and return matching entries based in the filter predicate provided.
+     * 
+     * @param {*} filter - A filter predicate, matching entries are returned.
+     * @returns {Array<T>} matching entries.
+     */
+    static select(filter) {
+        if (!this.data) {
+            throw new Error("Data source not initialised.");
+        }
 
-// --- MOCK MUST COME FIRST ---
-jest.mock("../models/CompanyModel.mjs", () => ({
-  __esModule: true,
-  default: {
-    select: jest.fn(),
-    setDataSource: jest.fn()
-  }
-}));
+        if (typeof filter === "function") {
+            return this.data.filter(filter).map(e => e.clone())
+        } else {
+            return this.data.map(e => e.clone())
+        }
+    }
+    
+    /**
+     * Updates matching entries based in the filter predicate provided.
+     * 
+     * @param {*} filter - A filter predicate, matching entries are deleted.
+     * @param {*} entry - The updated entry.
+     * @returns the number of deleted entries
+     */
+    static update(filter, entry) {
+        if (!this.data) {
+            throw new Error("Data source not initialised.");
+        }
 
-import { CompanyController } from "../controllers/CompanyController.mjs";
-import CompanyModel from "../models/CompanyModel.mjs";
+        if (typeof filter !== "function") {
+            throw new Error("Filter must be a predicate function.");
+        }
 
-describe("CompanyController.viewCompanies", () => {
+        let count = 0;
+        for (let index = 0; index < this.data.length; index++) {
+            if (filter(this.data[index])) {
+                this.data[index] = entry.clone();
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    /**
+     * 
+     * @param {any} entry - The entry object to insert into the data store.
+     * @returns 
+     */
+    static insert(entry) {
+        if (!this.data) {
+            throw new Error("Data source not initialised.");
+        }
 
-  let req;
-  let res;
+        this.data.push(entry.clone());
+    }
 
-  beforeEach(() => {
-    req = {};
+    /**
+     * Delete matching entries based in the filter predicate provided.
+     * 
+     * @param {*} filter - A filter predicate, matching entries are deleted.
+     * @returns the number of deleted entries
+     */
+    static delete(filter) {
+        if (!this.data) {
+            throw new Error("Data source not initialised.");
+        }
 
-    res = {
-      status: jest.fn().mockReturnThis(),
-      setHeader: jest.fn(),
-      json: jest.fn()
-    };
-  });
+        if (typeof filter !== "function") {
+            throw new Error("Filter must be a predicate function.");
+        }
 
-  it("returns 200 and the list of companies", () => {
-    const mockCompanies = [
-      { name: "DreamBuild Homes", basePrice: 170000, rating: 4.5 },
-      { name: "Value Builders", basePrice: 150000, rating: 4.0 }
-    ];
-
-    CompanyModel.select.mockReturnValue(mockCompanies);
-
-    CompanyController.viewCompanies(req, res);
-
-    expect(CompanyModel.select).toHaveBeenCalledTimes(1);
-
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.setHeader).toHaveBeenCalledWith("Content-Type", "application/json");
-
-    expect(res.json).toHaveBeenCalledWith({
-      message: "records retrieved",
-      data: mockCompanies
-    });
-  });
-
-});
+        const countBefore = this.data.length;
+        this.data = this.data.filter(entry => !filter(entry));
+        return countBefore - this.data.length;
+    }
+    
+    clone() {
+        return Object.assign(Object.create(Object.getPrototypeOf(this)), this)
+    }
+}
